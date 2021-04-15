1df16c553e431576e393081a531e985106ac1e28
Codeliner <codeliner96@gmail.com>
2021-04-15 09:11:13 +0900

    Added song add form

--- src/views/playlists/PlaylistDetail.vue
+++ src/views/playlists/PlaylistDetail.vue
@@ -15,26 +15,56 @@
     </div>
     <!-- Right side (Songs) -->
     <div class="w-3/5">
-      <button>Add song</button>
+      <button class="mb-6" @click="handleShowFormToggle">
+        {{ AddSongButtonText }}
+      </button>
+      <!-- note. Add song form -->
+      <form class="w-full" v-if="showForm">
+        <input type="text" placeholder="Title" v-model="songTitle" required />
+        <input type="text" placeholder="Artist" v-model="songArtist" required />
+        <button>Add</button>
+      </form>
       <div v-if="playlist.songs.length">{{ playlist.songs }}</div>
     </div>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 import { useRoute } from "vue-router";
 import getDocument from "../../composables/getDocument";
 
 export default {
   setup() {
     const route = useRoute();
+    const AddSongButtonText = ref("Add song");
+    const songTitle = ref("");
+    const songArtist = ref("");
+    const showForm = ref(false);
+
+    const handleShowFormToggle = () => {
+      showForm.value = !showForm.value;
+      if (showForm.value) {
+        AddSongButtonText.value = "Cancel";
+      } else {
+        AddSongButtonText.value = "Add song";
+      }
+    };
 
     const { document: playlist, error: playlistError } = getDocument(
       "playlists",
       route.params.id
     );
 
-    return { playlist, playlistError };
+    return {
+      playlist,
+      playlistError,
+      AddSongButtonText,
+      songTitle,
+      songArtist,
+      showForm,
+      handleShowFormToggle,
+    };
   },
 };
 </script>
